01cd8d8647dad759f367262058367f70033774f7
Bilb <audric.bilb@gmail.com>
2012-06-10 14:04:37 +0200

    Mise à jour TODO

--- Main.java
+++ Main.java
@@ -30,7 +30,7 @@ import org.w3c.dom.events.MutationEvent;
 /* Choses à implémenter:
  *		- mutationDeliverySequence (fusion et fission), attention à prendre en compte capacité du transporter. voir fission de production
  *		- Opérateurs de croisement
- *		- AE : 
+ *		- AE : cd TP
  *		- operateur d'egalité entre 2 solutions
  *		- operateur de remplacement, il faut se renseigner, voir TP AE
  *		- optimiser : fonction :  boucle principale, effectuant un nombre de générations + d'autres critères d'arrêt à implementer.
